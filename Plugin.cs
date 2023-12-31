9140fa46f0dbc5bb5b7c667a414fefa0be04b9a8
TimShaw1 <timothyshaw0225@gmail.com>
2023-12-31 16:02:49 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using LC_API;
+using System.Linq;
+using UnityEngine;
+
+// StartOfRound requires adding the game's Assembly-CSharp to dependencies
+
+namespace Wendigos
+{
+    public class WendigoAI : MaskedPlayerEnemy
+    {
+        public override void Update()
+        {
+            base.Update();
+            {
+
+            }
+        }
+    }
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            StartOfRound startOfRound = StartOfRound.Instance;
+
+            var currentLevel = RoundManager.Instance.currentLevel;
+
+            RoundManager.Instance.currentLevel.Enemies.Add(new SpawnableEnemyWithRarity());
+            
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
