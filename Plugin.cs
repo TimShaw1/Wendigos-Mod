1ac7ea61139e9a1de5c1eeb779e492d770c0cc42
TimShaw1 <timothyshaw0225@gmail.com>
2024-01-28 18:03:29 -0500

    fix hash function

--- Plugin.cs
+++ Plugin.cs
@@ -496,14 +496,6 @@ namespace Wendigos
             }
         }
 
-        public static string GetHashSHA1(this byte[] data)
-        {
-            using (var sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider())
-            {
-                return string.Concat(sha1.ComputeHash(data).Select(x => x.ToString("X2")));
-            }
-        }
-
         static AudioClip LoadWavFile(string audioFilePath)
         {
             if (File.Exists(audioFilePath))
