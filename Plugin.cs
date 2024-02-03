d3af996914edf54760e2d044d58828eb6a64cf05
TimShaw1 <timothyshaw0225@gmail.com>
2024-02-02 21:09:25 -0500

    t

--- Plugin.cs
+++ Plugin.cs
@@ -224,7 +224,7 @@ namespace Wendigos
                         // This is a server-only method that will broadcast the named message.
                         // Caution: Invoking this method on a client will throw an exception!
                         //WriteToConsole("Sending Message...");
-                        customMessagingManager.SendNamedMessageToAll(MessageName, writer, NetworkDelivery.ReliableFragmentedSequenced);
+                        customMessagingManager.SendNamedMessageToAll(MessageName, writer, NetworkDelivery.Reliable);
                         //WriteToConsole("Sent Message");
                     }
                     else
@@ -232,7 +232,7 @@ namespace Wendigos
                         // This is a client or server method that sends a named message to one target destination
                         // (client to server or server to client)
                         //WriteToConsole("Sending Message...");
-                        customMessagingManager.SendNamedMessage(MessageName, NetworkManager.ServerClientId, writer, NetworkDelivery.ReliableFragmentedSequenced);
+                        customMessagingManager.SendNamedMessage(MessageName, NetworkManager.ServerClientId, writer, NetworkDelivery.Reliable);
                         //WriteToConsole("Sent Message");
                     }
                 }
